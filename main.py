90eff0b4ebaa71ea2126a91cbb55d7709292d93b
Amarnath C <amarnathcharichilil@gmail.com>
2021-09-07 19:37:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from flask import Flask
-from flask import request, jsonify
+from flask import request, jsonify, redirect
 from os import environ as e
 app = Flask("neko")
 
+@app.route('/')
+def redirect ():
+ return redirect("https://roseloverx.me")
+
+@app.route('/screenshot')
+def ss():
+ q = request.args.get("url")
+ if not q:
+   return jsonify({"status": 400, "error": "url parameter not provided."})
+ 
+
 def ping():
  return jsonify({"status": "ok", "author": "RoseLovErX"})
+
 
 app.add_url_rule("/ping", "ping", ping, methods=['GET'])
 
