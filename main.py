699ae06b2983c3a2760adbdc223f9531d9c02616
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-09-07 14:19:50 +0000

    RoseLoverX: auto fixes

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-from flask import Flask
-from flask import request, jsonify, redirect
 from os import environ as e
+
+from flask import Flask, jsonify, redirect, request
+
 app = Flask("neko")
 
 api = "rlx"
 
-@app.route('/')
-def redirect ():
- return redirect("https://roseloverx.me")
 
-@app.route('/screenshot')
+@app.route("/")
+def redirect():
+    return redirect("https://roseloverx.me")
+
+
+@app.route("/screenshot")
 def ss():
- q = request.args.get("url")
- if not q:
-   return jsonify({"status": 400, "error": "url parameter not provided."})
- 
+    q = request.args.get("url")
+    if not q:
+        return jsonify({"status": 400, "error": "url parameter not provided."})
+
 
 def ping():
- return jsonify({"status": "ok", "author": "RoseLovErX"})
+    return jsonify({"status": "ok", "author": "RoseLovErX"})
 
 
-app.add_url_rule("/ping", "ping", ping, methods=['GET'])
+app.add_url_rule("/ping", "ping", ping, methods=["GET"])
 
-app.run(host='0.0.0.0', port=e.get("PORT"), threaded=True)
+app.run(host="0.0.0.0", port=e.get("PORT"), threaded=True)
