bffecb9aac139fb574dd98340a0d3adc66d14d5d
Amarnath C <amarnathcharichilil@gmail.com>
2021-09-26 22:48:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,45 +71,8 @@ def ping():
 
 app.add_url_rule("/ping", "ping", ping, methods=["GET"])
 
-import contextlib
-
-from lona import LonaApp, LonaView
-from lona.html import H1, HTML, Button, NumberInput, Span
-
-app2 = LonaApp("lona")
 
 
-@app2.route("/")
-class CounterView(LonaView):
-    def handle_request(self, request):
-        counter = Span("0")
-        number_input = NumberInput(value=10, _style={"width": "3em"})
-        html = HTML(
-            H1("Counter: ", counter),
-            number_input,
-            Button("Set", _id="set"),
-            Button("Decrease", _id="decrease", _style={"margin-left": "1em"}),
-            Button("Increase", _id="increase"),
-        )
-
-        while True:
-            self.show(html)
-
-            input_event = self.await_click()
-            if input_event.node_has_id("increase"):
-                counter.set_text(
-                    int(counter.get_text()) + 1,
-                )
-            elif input_event.node_has_id("decrease"):
-                counter.set_text(
-                    int(counter.get_text()) - 1,
-                )
-            elif input_event.node_has_id("set"):
-                with contextlib.suppress(TypeError):
-                    counter.set_text(int(number_input.value))
-
-
-app2.run(port=e.get("PORT"), host="0.0.0.0")
 app.run(host="0.0.0.0", port=e.get("PORT"), threaded=True)
 
 # later
