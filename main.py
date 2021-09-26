fa5eb4ce2cf67d4374f7a4c9d7fce95171697fdc
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-09-26 17:26:28 +0000

    RoseLoverX: auto fixes

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import contextlib
 
 from lona import LonaApp, LonaView
-from lona.html import H1, HTML, Button, NumberInput, Span
-from lona.html import Strong, THead, TBody, Table, HTML, Tr, Th, Td, H1
+from lona.html import (
+    H1,
+    HTML,
+    Button,
+    NumberInput,
+    Span,
+    Strong,
+    Table,
+    TBody,
+    Td,
+    Th,
+    THead,
+    Tr,
+)
 
 app = LonaApp("lona")
 
@@ -37,37 +49,38 @@ class CounterView(LonaView):
                     counter.set_text(int(number_input.value))
 
 
-
 from lona_bootstrap_5 import (
-    SecondaryButton,
-    PrimaryButton,
     DangerButton,
-    Progress,
     Modal,
+    PrimaryButton,
+    Progress,
+    SecondaryButton,
 )
 
-
 NAMES = [
-    'Anjana',
-    'TamilVip',
-    'RexModz',
-    'PiroNoob',
-    'RoseLoverX',
-    'Noobmaster',
-    'Drdrex',
-    'Lumcas',
-    'JackyXD',
-    'OnePunchMan',
+    "Anjana",
+    "TamilVip",
+    "RexModz",
+    "PiroNoob",
+    "RoseLoverX",
+    "Noobmaster",
+    "Drdrex",
+    "Lumcas",
+    "JackyXD",
+    "OnePunchMan",
 ]
 
-app.add_static_file('lona/style.css', """
+app.add_static_file(
+    "lona/style.css",
+    """
     body {
         margin: 1em;
     }
-""")
+""",
+)
 
 
-@app.route('/')
+@app.route("/")
 class LonaBootstrap5PopupView(LonaView):
 
     # popup callbacks #########################################################
@@ -80,25 +93,25 @@ class LonaBootstrap5PopupView(LonaView):
 
     def open_delete_popup(self, input_event):
         with self.html.lock:
-            self.selected_tr = input_event.node.closest('tr')
+            self.selected_tr = input_event.node.closest("tr")
             name = self.selected_tr.nodes[0].get_text()
 
             # close button
             self.modal.handle_click = self.close_popup
 
             # modal content
-            self.modal.set_title('Delete')
+            self.modal.set_title("Delete")
 
             self.modal.set_body(
-                'Do you really want to delete ',
+                "Do you really want to delete ",
                 Strong(name),
-                '?',
+                "?",
             )
 
             # modal buttons
             self.modal.set_buttons(
-                DangerButton('Delete', handle_click=self.delete),
-                SecondaryButton('Cancel', handle_click=self.close_popup),
+                DangerButton("Delete", handle_click=self.delete),
+                SecondaryButton("Cancel", handle_click=self.close_popup),
             )
 
             # show popup
@@ -109,43 +122,39 @@ class LonaBootstrap5PopupView(LonaView):
         self.modal = Modal(centered=True)
 
         self.html = HTML(
-            H1('Confirmation Window'),
-
+            H1("Confirmation Window"),
             Progress(
                 value=0,
                 _style={
-                    'width': '30em',
-                    'float': 'left',
-                    'margin-top': '0.5em',
-                    'margin-right': '1em',
+                    "width": "30em",
+                    "float": "left",
+                    "margin-top": "0.5em",
+                    "margin-right": "1em",
                 },
             ),
-
-            PrimaryButton('Generate Name List', _id='generate'),
-
+            PrimaryButton("Generate Name List", _id="generate"),
             Table(
-                _class='table',
+                _class="table",
                 nodes=[
                     THead(
                         Tr(
-                            Th('Name', width='50%'),
-                            Th('Action', width='50%'),
+                            Th("Name", width="50%"),
+                            Th("Action", width="50%"),
                         ),
                     ),
                     TBody(),
                 ],
             ),
-
             self.modal,
         )
 
         # generate name list
-        generate_button = self.html.query_selector('#generate')
+        generate_button = self.html.query_selector("#generate")
 
         self.await_click(generate_button, html=self.html)
 
-        progress = self.html.query_selector('.progress')
-        tbody = self.html.query_selector('tbody')
+        progress = self.html.query_selector(".progress")
+        tbody = self.html.query_selector("tbody")
 
         for index, name in enumerate(NAMES):
             progress.set_percentage((index + 1) * 10)
@@ -155,7 +164,7 @@ class LonaBootstrap5PopupView(LonaView):
                     Td(name),
                     Td(
                         DangerButton(
-                            'Delete',
+                            "Delete",
                             handle_click=self.open_delete_popup,
                         ),
                     ),
@@ -168,5 +177,6 @@ class LonaBootstrap5PopupView(LonaView):
         generate_button.disabled = True
 
         return self.html
+
 
 app.run(port=e.get("PORT"), host="0.0.0.0")
