b7add94d20fd0566b308151d01a6f96980b107ce
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-09-26 16:53:39 +0000

    RoseLoverX: auto fixes

--- lona.py
+++ lona.py
@@ -1,25 +1,24 @@
 import contextlib
 import os
-from lona.html import NumberInput, Button, Span, HTML, H1
-from lona import LonaView, LonaApp
+
+from lona import LonaApp, LonaView
+from lona.html import H1, HTML, Button, NumberInput, Span
 
 app = LonaApp(__file__)
 
 
-@app.route('/counter')
+@app.route("/counter")
 class CounterView(LonaView):
     def handle_request(self, request):
-        counter = Span('0')
-        number_input = NumberInput(value=10, _style={'width': '3em'})
+        counter = Span("0")
+        number_input = NumberInput(value=10, _style={"width": "3em"})
 
         html = HTML(
-            H1('Counter: ', counter),
-
+            H1("Counter: ", counter),
             number_input,
-            Button('Set', _id='set'),
-
-            Button('Decrease', _id='decrease', _style={'margin-left': '1em'}),
-            Button('Increase', _id='increase'),
+            Button("Set", _id="set"),
+            Button("Decrease", _id="decrease", _style={"margin-left": "1em"}),
+            Button("Increase", _id="increase"),
         )
 
         while True:
@@ -28,19 +27,19 @@ class CounterView(LonaView):
             input_event = self.await_click()
 
             # increase
-            if input_event.node_has_id('increase'):
+            if input_event.node_has_id("increase"):
                 counter.set_text(
                     int(counter.get_text()) + 1,
                 )
 
             # decrease
-            elif input_event.node_has_id('decrease'):
+            elif input_event.node_has_id("decrease"):
                 counter.set_text(
                     int(counter.get_text()) - 1,
                 )
 
             # set
-            elif input_event.node_has_id('set'):
+            elif input_event.node_has_id("set"):
                 with contextlib.suppress(TypeError):
                     counter.set_text(int(number_input.value))
 
