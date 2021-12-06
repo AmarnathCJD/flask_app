556f5560dc404e155b4afcc54be6f321e0093f69
Amarnath C <41010786@sfcollege.edu>
2021-12-06 19:36:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+import os
 from telethon import TelegramClient
 
 routes = web.RouteTableDef()
@@ -16,3 +17,16 @@ bot.start()
 @routes.get("/")
 async def uu(r):
     return web.Response(text=str(bot.get_me()))
+
+async def start_server():
+    port = int(os.environ.get('PORT'))
+    app = web.Application()
+    app.add_routes(routes)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    await web.TCPSite(runner, "0.0.0.0", port).start()
+    
+# Server Startup
+asyncio.get_event_loop().run_until_complete(start_server())
+print('Web Server Started.')
+bot.run_until_completed()
