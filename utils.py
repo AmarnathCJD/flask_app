13fd23da0e59d03d18b172e479eba49eda6ddbc6
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-08 11:20:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -91,6 +91,7 @@ def stripe_check(cc_num, exp_mo, exp_year, cvv):
 def paste(text):
     url = "https://nekobin.com/api/documents"
     r = post(url, json={"content": text})
+    print(r.json())
     try:
         return r.json()
     except:
